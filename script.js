1cf4f9925548b80e4e728e5c74332301d0ba3cff
raydover <105178034+raydover@users.noreply.github.com>
2022-05-26 17:42:50 -0400

    back to starting initial HTML, clear JS start over

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
 var state = 'quiz';
 
-var questions = [{
-    title: "Insert Question?",
-    choices: ["option( )", "option( )", "optiona( )", "option( )"],
-    answer: "answer( )"
-},
-{
-    title: "Insert Question?",
-    choices: ["option( )", "option( )", "optiona( )", "option( )"],
-    answer: "answer( )"
-},
-{
-    title: "Insert Question?",
-    choices: ["option( )", "option( )", "optiona( )", "option( )"],
-    answer: "answer( )"
-},
-{
-    title: "Insert Question?",
-    choices: ["option( )", "option( )", "optiona( )", "option( )"],
-    answer: "answer( )"
-}
 
-]
+
 
 
 function init() {
     displayState();
 }
+
+
+
+
 
 
 
