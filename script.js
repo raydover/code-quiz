b400d4419dce76e02a2b3f3816a4f298fc59ee4c
raydover <105178034+raydover@users.noreply.github.com>
2022-05-26 17:55:54 -0400

    create questions, answers section js file

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ var state = 'quiz';
 
 
 
+startButton.onclick = function () {
+    displayBox.classList.add("activeDisplayBox");
+}
 
+quitButton.onclick = function () {
+    displayBox.classList.remove("activeDisplayBox");
+}
 
 function init() {
     displayState();
