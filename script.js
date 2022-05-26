d99ba68e447dd4779d21015006a4a91ba7c2e254
raydover <105178034+raydover@users.noreply.github.com>
2022-05-26 15:02:26 -0400

    var statements JS

---
+++ script.js
@@ -0,0 +1,30 @@
+var state = 'start';
+
+var highScoreEl = document.getElementById;('#highScore');
+var quizHighScoreEl = document.getElementById;('#quizHighScore');
+
+var timeEl = document.getElementById;('#timer');
+var timeRemainEl = document.getElementById;('#timeRemain');
+
+var quizEl = document.getElementById;('#quiz');
+var titleEl = document.getElementById;('#title');
+
+var questionsEl = document.getElementById;('#questions');
+var questionButtonsEl = document.getElementById;('#questionButtons');
+
+var scoreEl = document.getElementById;('#score');
+var quizScoreEl = document.getElementById;('#quizScore');
+
+var endEL = document.getElementById;('#end');
+
+var timeremaining;
+timeEl.innerHTML = 0;
+
+funtion.init(){
+
+}
+
+
+
+
+init ();
