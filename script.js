7dd15833ab054ea2ec284afea3ab8a276df50f94
raydover <105178034+raydover@users.noreply.github.com>
2022-06-01 23:43:32 -0400

    set timer function, js

--- script.js
+++ script.js
@@ -4,47 +4,46 @@ var state = 'start';
 var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var endEl = document.querySelector("#end");
-var questionsEL = document.querySelector("#questions");
+var questionsEl = document.querySelector("#questions");
 var startBtn = document.querySelector("#start button");
-var quizTitle = document.querySelector('#quiz #title');
-
-
-var secondsLeft = 30;
-var position = 0;
+var quizTitle = document.querySelector("#quiz #title");
+intialsInput = document.querySelector("#initials");
+var cursor = 0;
+var timerInterval;
 
 // List of questions, answers and correct answers for variable quiz questions
-var quizQuestions = [
+var questions = [
     {
         title: "What coding language is at the heart of every website and the standard language used to build the structure of a webpage?",
         possible: ["Java Script", "HTML", "CSS", "JQuery", "HTML"],
-        correctAnswer: "HTML",
+        correct: "HTML",
     },
     {
         title: "What is used to add styling to the elements created with HTML?",
         answer: ["GitHub", "Bootstrap", "JSON", "CSS"],
-        correctAnswer: "CSS",
+        correct: "CSS",
     },
     {
         title: "What is a widely used scripting language that adds functionality and interactivity to a webpage?",
         answer: ["APIs", "Java Script", "DOM", "VS Code"],
-        correctAnswer: "Java Script",
+        correct: "Java Script",
     },
     {
         title: "What programming interface allows us to use JavaScript to interact with HTML elements?",
         answer: ["HTML", "JSON", "DOM", "Terminal"],
-        correctAnswer: "DOM",
+        correct: "DOM",
     },
     {
         title: "What term means that you meet the minimum requirements to apply for a job as a web developer?",
         answer: ["employer-ready", "employer-competitive", "employer-minimum", "employer-required"],
-        correctAnswer: "employer-ready",
+        correct: "employer-ready",
     },
     {
         title: "What term means that you have given yourself the best chance to secure your desired job?",
         answer: ["employer-desired", "employer-best", "employer-secure", "employer-competitive"],
-        correctAnswer: "employer-competitive",
+        correct: "employer-competitive",
     }
-]
+];
 
 // Sets function to display state and question, if statements to start quiz, and when the quiz has ended
 function displayState() {
@@ -58,24 +57,25 @@ function displayState() {
         quizEl.style.display = 'block';
         endEl.style.display = 'none';
         displayQuestion();
-        displayState();
     }
     if (state === 'end') {
         startEl.style.display = 'none';
         quizEl.style.display = 'none';
         endEl.style.display = 'block';
-        displayState();
     }
 }
 
 // Sets function to diplay quiz questions, query quiz title, for loop possible, create button, update button text content, append buttons to question
 function displayQuestion() {
-    var question = quizQuestions[0];
+    var question = questions[cursor];
+
+    questionsEl.innerHTML = null;
     quizTitle.textContent = question.title;
+
     question.possible.forEach(function (item) {
         var btnEl = document.createElement('button');
         btnEl.textContent = item;
-        questionsEL.appendChild(btnEl);
+        questionsEl.appendChild(btnEl);
     });
 }
 
@@ -90,28 +90,37 @@ startBtn.addEventListener("click", function () {
 });
 
 questionsEl.addEventListener("click", function (event) {
-    if (event.target.type == "submit") {
-        if (event.target.texContent === quizQuestions[position].correctAnswer) {
+    var element = event.target;
+    if (element.matches('button')) {
+
+        // Verify the corect answer
+        var children = element.parentElement.children;
+        var position = Array.from(children).indexOf(element);
+
+        if (position === questions[cursor].correct) {
         } else {
-            secondsLeft = secondsLeft - 5;
         }
 
-        position++;
+        cursor++;
 
-        if (position <= quizQuestions.length) {
+        if (cursor >= questions.length) {
             state = 'end';
-            displayQuestions();
-            clearInterval();
             displayState();
         } else {
-            displayState();
+            displayQuestion();
         }
     }
+});
+
+endEl.addEventListener('submit', function (event) {
+    event.preventDefault();
+    localStorage.setItem('highScores', intialsInput.value);
 });
 
 init();
 
 
+// var secondsLeft = 30;
 
 // // Funtion sets up the timer, messages, If statement, quiz end, etc.. 
 // function displayTimer() {
