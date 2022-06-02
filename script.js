0d2532222560a26deea67402a7d3b49433b8c30c
raydover <105178034+raydover@users.noreply.github.com>
2022-06-01 21:37:39 -0400

    commit changes made to quiz questions, JS file

--- script.js
+++ script.js
@@ -6,74 +6,45 @@ var startEl = document.querySelector("#start");
 var quizEl = document.querySelector("#quiz");
 var endEl = document.querySelector("#end");
 var startBtn = document.querySelector("#start button");
-var quizTitle =document.querySelector('#quiz #title');
+var quizTitle = document.querySelector('#quiz #title');
 
 var secondsLeft = 30;
 var position = 0;
 
-// List of questions, answers and correct answers for the quiz
+// List of questions, answers and correct answers for variable quiz questions
 var quizQuestions = [
     {
         question: "What coding language is at the heart of every website and the standard language used to build the structure of a webpage?",
-        possible: [
-            optionA: "Java Script",
-            optionB: "HTML",
-            optionC: "CSS",
-            optionD: "JQuery"
+        possible: ["Java Script", "HTML", "CSS", "JQuery", "HTML"],
         correctAnswer: "HTML",
-        ]
     },
     {
         question: "What is used to add styling to the elements created with HTML?",
+        answer: ["GitHub", "Bootstrap", "JSON", "CSS"],
         correctAnswer: "CSS",
-        answer: [
-            optionA: "GitHub",
-            optionB: "Bootstrap",
-            optionC: "JSON",
-            optionD: "CSS"
-        ]
     },
     {
         question: "What is a widely used scripting language that adds functionality and interactivity to a webpage?",
+        answer: ["APIs", "Java Script", "DOM", "VS Code"],
         correctAnswer: "Java Script",
-        answer: [
-            optionA: "APIs",
-            optionB: "Java Script",
-            optionC: "DOM",
-            optionD: "VS Code"
-        ]
     },
     {
         question: "What programming interface allows us to use JavaScript to interact with HTML elements?",
+        answer: ["HTML", "JSON", "DOM", "Terminal"],
         correctAnswer: "DOM",
-        answer: [
-            optionA: "HTML",
-            optionB: "JSON",
-            optionC: "DOM",
-            optionD: "Terminal"
-        ]
     },
     {
         question: "What term means that you meet the minimum requirements to apply for a job as a web developer?",
+        answer: ["employer-ready", "employer-competitive", "employer-minimum", "employer-required"],
         correctAnswer: "employer-ready",
-        answer: [
-            optionA: "employer-ready",
-            optionB: "employer-competitive",
-            optionC: "employer-minimum",
-            optionD: "employer-required"
-        ]
     },
     {
         question: "What term means that you have given yourself the best chance to secure your desired job?",
+        answer: ["employer-desired", "employer-best", "employer-secure", "employer-competitive"],
         correctAnswer: "employer-competitive",
-        answer: [
-            optionA: "employer-desired",
-            optionB: "employer-best",
-            optionC: "employer-secure",
-            optionD: "employer-competitive"
-        ]
     }
 ]
+
 
 function displayState() {
     if (state === 'start') {
@@ -100,24 +71,6 @@ function displayState() {
 function displayQuestions() {
     questionsEl.innerHTML = "";
     answerEl.innerHTML = "";
-
-    // Variable button a, b, c, d elements
-    var optionA = document.querySelector("button")
-    var optionB = document.querySelector("button")
-    var optionC = document.querySelector("button")
-    var optionD = document.querySelector("button")
-
-    viewQuestions.textContent = quizQuestions[position].question;
-    optionA.textContent = quizQuestions[position].optionA;
-    optionB.textContent = quizQuestions[position].optionB;
-    optionC.textContent = quizQuestions[position].optionC;
-    optionD.textContent = quizQuestions[position].optionD;
-
-    questionsEl.appendChild(viewQuestions);
-    answerEl.appendChild(optionA);
-    answerEl.appendChild(optionB);
-    answerEl.appendChild(optionC);
-    answerEl.appendChild(optionD);
 
 }
 
@@ -174,3 +127,23 @@ questionsEl.addEventListener("click", function (event) {
 init();
 
 
+
+
+
+// Variable button a, b, c, d elements
+// var optionA = document.querySelector("button")
+// var optionB = document.querySelector("button")
+// var optionC = document.querySelector("button")
+// var optionD = document.querySelector("button")
+
+// viewQuestions.textContent = quizQuestions[position].question;
+// optionA.textContent = quizQuestions[position].optionA;
+// optionB.textContent = quizQuestions[position].optionB;
+// optionC.textContent = quizQuestions[position].optionC;
+// optionD.textContent = quizQuestions[position].optionD;
+
+// questionsEl.appendChild(viewQuestions);
+// answerEl.appendChild(optionA);
+// answerEl.appendChild(optionB);
+// answerEl.appendChild(optionC);
+// answerEl.appendChild(optionD);
